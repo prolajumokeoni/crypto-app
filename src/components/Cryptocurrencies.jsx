cbc4c4962a45fd1691af2c8f2b93b01437477084
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2022-04-22 09:09:20 +0100

    Add search button

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import millify  from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
@@ -8,10 +8,18 @@ const Cryptocurrrencies = ({ simplified }) => {
 	const count = simplified ? 10 : 100;
 	const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
 	const [cryptos, setCryptos] = useState(cryptosList ?.data?.coins);
-	console.log(cryptos);
+	const [searchTerm, setSearchTerm] = useState('');	
+	useEffect(() => {
+
+			const filteredData = cryptosList ?.data ?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+			setCryptos(filteredData);
+	}, [cryptosList, searchTerm]);
 	if(isFetching) return 'loading ...';
 	return (
 		<>
+		<div className='search-crypto'>
+				<Input  placeholder='Search Cryptocurrency' onChange={(e) => setSearchTerm(e.target.value)} />
+		</div>
 			<Row gutter={[32, 32]} className="crypto-card-container">
 				{cryptos ?.map((currency) => (
 					 <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id} >
