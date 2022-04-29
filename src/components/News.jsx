be60adf6293edd20a4326c6259643d1fbb5f2768
Oni Olajumoke Priscilla <prolajumokeoni@gmail.com>
2022-04-29 21:11:01 +0100

    Fetch news and add search bar

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,31 +1,79 @@
-import React from 'react'
-import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
-import moment from 'moment';
-import { useGetCryptosNewsQuery } from '../services/cryptoNewsApi';
+import React, { useState } from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
+import { useGetCryptosNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi"
 const { Text, Title } = Typography;
 const { Option } = Select;
-const demoImage = 'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+const demoImage =
+  "https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News";
 
 const News = ({ simplified }) => {
-	const { data: cryptoNews } = useGetCryptosNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 });
-	if(!cryptoNews ?.value) return `Loading...`;
-	return (
-		<Row gutter={[ 24, 24] }>
-		{cryptoNews.value.map((news, i) => (
-			<Col xs={24} sm={12} lg={8} key={i}>
-				<Card hoverable className='news-card'>
-					<a href={news.url} target="_blank" rel="noreferrer">
-						<div className="news-image-container">
-							<Title className='news-title' level={4}>{news.name}</Title>
-							<img src={news ?.image ?.thumbnail ?.contentUrl || demoImage } alt="news" />
-						</div>
-						<p>{news.description.length > 100 ? `${news.description.substring(0, 100)}...` : news.description}</p>
-					</a>
-				</Card>
-			</Col>
-		))}
-		</Row>
-	)
-}
+	const [newsCategory, setNewsCategory] = useState('Cryptocurrency')
+  const { data: cryptoNews } = useGetCryptosNewsQuery({
+    newsCategory,
+    count: simplified ? 6 : 12,
+  });
 
-export default News
+	const { data } = useGetCryptosQuery(100);
+  if (!cryptoNews?.value) return `Loading...`;
+  return (
+    <Row gutter={[24, 24]}>  
+      {!simplified && (
+        <Col span={24}>
+          <Select
+            showSearch
+            className="select-news"
+            placeholder="Select a Crypto"
+            optionFilterProp="children"
+            onChange={(value) => setNewsCategory(value)}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+          >
+            <Option value="Cryptocurency">Cryptocurrency</Option>
+            {data?.data?.coins?.map((currency) => <Option value={currency.name}>{currency.name}</Option>)}
+          </Select>
+        </Col>
+      )}
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+                <img
+                  src={news?.image?.thumbnail?.contentUrl || demoImage}
+                  alt="news"
+                  className="imgNews"
+                />
+              </div>
+              <p>
+                {news.description.length > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar
+                    src={
+                      news.provider[0]?.image?.thumbnail?.contentUrl ||
+                      demoImage
+                    }
+                    alt="news"
+                  />
+									<Text className="provider-name">{news.provider[0] ?.name}</Text>
+                </div>
+                  <Text>
+                    {moment(news.datePublished).startOf("ss").fromNow()}
+                  </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default News;
